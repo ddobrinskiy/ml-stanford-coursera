93a0ab08e57010b8d3751c1251350c53e0903cf6
starm3nace <nomail@singularity.blackhole>
2017-08-12 20:10:08 +0300

    param choice work in progress

--- machine-learning-ex6/dataset3Params.m
+++ machine-learning-ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,16 +15,40 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
+param_grid = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+for i=1:size(param_grid)(2)
+    for j=1:size(param_grid)(2)
+        fprintf('i=%d\nj=%d\n==================', i, j)
+    end
+end
+
+
+for i = 1:2
+    for j = 1:2
+        C = param_grid(i);
+        sigma = param_grid(j);
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+        predictions = svmPredict(model, Xval);
+        mean(double(predictions ~= yval))
+    end
+end
 
 
 
+C=100;
+size(Xval)
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+predictions = svmPredict(model, Xval);
+size(predictions)
+mean(double(predictions ~= yval))
+0
 
 
 
