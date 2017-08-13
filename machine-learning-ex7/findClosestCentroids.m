474e20ffcc5603882ee101a4f0297ec4dafda324
starm3nace <nomail@singularity.blackhole>
2017-08-13 19:45:02 +0300

    find closest centroid

--- machine-learning-ex7/findClosestCentroids.m
+++ machine-learning-ex7/findClosestCentroids.m
@@ -1,10 +1,10 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
-%
 
+%centroids=initial_centroids
 % Set K
 K = size(centroids, 1);
 
@@ -15,19 +15,28 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               cloupdasest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
-%
 
+% initalize structure to store distances from examples to all centroids
+%X = X(1:5, :)
+X_distances = [];
+for j = 1:K
+    % K = num of centroids;
 
-
-
-
-
-
-% =============================================================
-
+    c = centroids(j, :);
+    d = sum((X - c).^2, 2);
+    % iterate over each centroid adding a column to X_distances
+    % the new column contains m distances from x_i to current centroid_j
+    X_distances = [X_distances, d];
 end
 
+% Now X_distances contains distances from X_i (a row) to each of K centroids
+% (in the K columns).
+% Now we need to assign the closest centroid to each X_i, which is the index
+% of the minimum value row-wise
+[min_val, idx] = min(X_distances, [], 2);
+% =============================================================
+end
